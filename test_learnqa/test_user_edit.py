b1bdb18d837893969540868ed2b67dea6b3f399d
Aston <t.h.crush@mail.ru>
2022-12-30 09:15:13 +0100

    Only AQA_API changes

---
+++ test_learnqa/test_user_edit.py
@@ -0,0 +1,60 @@
+import requests
+from lib.assertions import Assertions
+from lib.base_case import BaseCase
+from lib.my_requests import MyRequests
+
+class TestUserEdit(BaseCase):
+    def test_edit_just_created_user(self):
+        # CREATE
+        register_data = self.prepare_registration_data()
+
+        response1 = MyRequests.post("/user/",
+            data=register_data)
+
+        Assertions.assert_status_code(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        password = register_data["password"]
+        first_name = register_data["firstName"]
+        print(first_name)
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {"email": email,
+                      "password": password
+                      }
+
+        response2 = MyRequests.post("/user/login",
+            data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+        print(response2.cookies)
+        print(response2.headers)
+        print(response2.text)
+
+        Assertions.assert_status_code(response2, 200)
+
+        # EDIT
+        new_name = "Artur"
+        response3 = MyRequests.put(f"/user/{user_id}",
+                                 cookies={"auth_sid": auth_sid},
+                                 headers={"x-csrf-token": token},
+                                 data={"firstName": new_name}
+                                 )
+        Assertions.assert_status_code(response3, 200)
+        print(response3.cookies)
+        print(response3.headers)
+        print(response3.text)
+
+        # CHECK
+
+        response4 = MyRequests.get(f"/user/{user_id}",
+                                 cookies={"auth_sid": auth_sid},
+                                 headers={"x-csrf-token": token}
+                                 )
+        print(response4.cookies)
+        print(response4.headers)
+        print(response4.text)
+        Assertions.assert_status_code(response4, 200)
+        Assertions.assert_json_value_by_name(response4, "firstName", new_name, f"First name is not changed after the edit")
---
+++ test_learnqa/test_user_register.py
@@ -0,0 +1,28 @@
+import requests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from lib.my_requests import MyRequests
+
+class TestUserRegister(BaseCase):
+    def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
+        data1 = self.prepare_registration_data(email)
+
+        response = MyRequests.post(
+            "/user/",
+            data=data1)
+        print(response.text)
+        print(response.status_code)
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f"Unexpected error message"
+
+    def test_create_user_successfully(self):
+        data2 = self.prepare_registration_data()
+        response = MyRequests.post(
+            "/api/user/",
+            data=data2)
+        print(response.text)
+        print(response.status_code)
+
+        Assertions.assert_json_has_key(response, "id")
+        Assertions.assert_status_code(response, 200)
